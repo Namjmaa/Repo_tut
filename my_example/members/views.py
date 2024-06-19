73f8b2121f3d333ba97e71f45ec1a7ff9b87aba4
Namjmaa <naagiidonama>
2024-06-19 16:09:17 +0800

    some changes

--- my_example/members/views.py
+++ my_example/members/views.py
@@ -10,3 +10,24 @@ def members(request):
     'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'x': ['Apple', 'Banana', 'Cherry'], 
+    'y': ['Apple', 'Banana', 'Cherry'], 
+  }
+  return HttpResponse(template.render(context, request))
